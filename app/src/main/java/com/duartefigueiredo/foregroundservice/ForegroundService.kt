0d1967c0511e78537e29e610d02327687d99cc4d
Duarte Figueiredo <duarte.figueiredo@outlook.com>
2019-09-29 12:03:55 +0100

    Initial commit

---
+++ app/src/main/java/com/duartefigueiredo/foregroundservice/ForegroundService.kt
@@ -0,0 +1,69 @@
+package com.duartefigueiredo.foregroundservice
+
+import android.R
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.Service
+import android.content.Intent
+import android.os.Build
+import android.os.IBinder
+import android.support.v4.app.NotificationCompat
+
+
+/**
+ * Copyright (c) 2019, CloudPear	.pt		.com  All rights reserved.
+ * Project ID: ForegroundService
+ * Workfile:
+ * Revision: 1.0.0
+ * Author: Duarte Figueiredo
+ * Date: 29/09/2019
+ */
+class ForegroundService : Service() {
+
+    companion object {
+        const val CHANNEL_ID = "1"
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
+        createNotificationChannel()
+
+        val notificationIntent = Intent(this, MainActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(
+            this,
+            0, notificationIntent, 0
+        )
+
+        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setContentTitle("Foreground Service")
+            .setSmallIcon(R.drawable.button_onoff_indicator_on)
+            .setContentIntent(pendingIntent)
+            .build()
+
+        startForeground(1, notification)
+
+        //do heavy work on a background thread
+
+
+        //stopSelf();
+
+        return START_NOT_STICKY
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val serviceChannel = NotificationChannel(
+                CHANNEL_ID,
+                "Foreground Service Channel",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+
+            val manager = getSystemService(NotificationManager::class.java)
+            manager.createNotificationChannel(serviceChannel)
+        }
+    }
+}
---
+++ app/src/main/java/com/duartefigueiredo/foregroundservice/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.duartefigueiredo.foregroundservice
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v4.content.ContextCompat
+import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var startForegroundButton: Button
+    private lateinit var stopForegroundButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        startForegroundButton = findViewById(R.id.startForegroundService)
+        startForegroundButton.setOnClickListener { startForegroundService() }
+
+        stopForegroundButton = findViewById(R.id.stopForegroundService)
+        stopForegroundButton.setOnClickListener { stopForegroundService() }
+    }
+
+    private fun startForegroundService() {
+        val serviceIntent = Intent(this, ForegroundService::class.java)
+
+        ContextCompat.startForegroundService(this, serviceIntent)
+    }
+
+    private fun stopForegroundService() {
+        val serviceIntent = Intent(this, ForegroundService::class.java)
+
+        stopService(serviceIntent)
+    }
+}
